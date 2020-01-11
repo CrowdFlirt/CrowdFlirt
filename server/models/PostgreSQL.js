d147d1166462113466f613018df5b10e85cfab87
Sayer Tindall <sayertindall@Sayers-MacBook-Pro.local>
2020-01-11 09:42:17 -0800

    server and db functional

---
+++ server/models/PostgreSQL.js
@@ -0,0 +1,16 @@
+const { Client } = require('pg');
+const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'sayertindall',
+    database: 'flirt',
+    password: '1111',
+});
+
+client.connect(err => {
+    if (err) {
+        console.error('connection error', err.stack)
+    } else {
+        console.log('Connected to PosntgreSQL')
+    }
+});
